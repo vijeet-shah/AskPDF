8d21ed347d5623cd81a9e300621bdea0416f6170
“Vijeet <vijeetbshah@gmail.com>
2023-12-15 13:20:10 +0530

    Prisma & MySQL Setup

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,16 +1,31 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user: any = getUser();
 
     if (!user.id || !user.email) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
-      // Database
-      return { sucess: true };
+      const dbUser = await db.user.findFirst({
+        where: {
+          id: user.id,
+        },
+      });
+
+      if (!dbUser) {
+        db.user.create({
+          data: {
+            id: user.id,
+            email: user.email,
+          },
+        });
+      }
+
+      return { success: true };
     }
   }),
 });
