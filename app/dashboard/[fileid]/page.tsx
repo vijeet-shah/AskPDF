2c2d28b0a0c256c55cd0f1ed268caa72bf3fa631
“Vijeet <vijeetbshah@gmail.com>
2024-01-08 21:30:13 +0530

    fix : Chat Section Completed

--- app/dashboard/[fileid]/page.tsx
+++ app/dashboard/[fileid]/page.tsx
@@ -1,4 +1,4 @@
-import ChatWrapper from "@/app/components/ChatWrapper";
+import ChatWrapper from "@/app/components/chat/ChatWrapper";
 import PdfRenderer from "@/app/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server";
@@ -27,6 +27,8 @@ const Page = async ({ params }: PageProps) => {
 
   if (!file) notFound();
 
+  const plan = await getUserSubscriptionPlan();
+
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
@@ -34,12 +36,12 @@ const Page = async ({ params }: PageProps) => {
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
             {/* Main area */}
-            <PdfRenderer />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper />
+          <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
         </div>
       </div>
     </div>
